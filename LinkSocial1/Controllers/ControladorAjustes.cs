ccf12fd884f151c0e9a6d9a8856155de8d70e38e
DESKTOP-NBUN2TS\Juanca <juancarlosdorado02c@gmail.com>
2024-02-19 01:39:45 +0100

    Version(modificar contraseña desde el perfil)

--- LinkSocial1/Controllers/ControladorAjustes.cs
+++ LinkSocial1/Controllers/ControladorAjustes.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DB;
+using LinkSocial1.DTO;
+using LinkSocial1.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace LinkSocial1.Controllers
 {
     public class ControladorAjustes : Controller
     {
+
+        private readonly GestorLinkSocialDbContext _contexto;
+
+        /// <summary>
+        /// Contructor con el contexto.
+        /// </summary>
+        /// <param name="contexto"></param>
+        public ControladorAjustes(GestorLinkSocialDbContext contexto)
+        {
+            _contexto = contexto;
+        }
         public IActionResult irAAjustes()
         {
             try
@@ -30,6 +45,43 @@ namespace LinkSocial1.Controllers
                 return View("~/Views/Errores/paginaError.cshtml");
             }
         }
-        
+
+        [HttpPost]
+        public IActionResult ActualizarContraseña(ModificarContraseñaDTO modelo)
+        {
+            try
+            {
+                ServicioEncriptar encriptarContraseña = new ServicioEncriptarImpl();
+
+                // Comprobamos que los datos del formulario están rellenos.
+                if (!ModelState.IsValid)
+                {
+                    return View("~/Views/Ajustes/ModificarContraseña.cshtml", modelo);
+                }
+                var claimsPrincipal = User;
+                string idUsuario = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+                // Busca al usuario por su ID en la base de datos.
+                var usuario = _contexto.Usuarios.FirstOrDefault(u => u.idUsuario == Convert.ToInt32(idUsuario));
+
+                // Si el usuario es distinto de null, actualiza la contraseña.
+                if (usuario != null)
+                {
+                    usuario.contraseña = encriptarContraseña.Encriptar(modelo.Password);
+
+                    _contexto.Entry(usuario).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    _contexto.SaveChanges();
+                }
+
+
+                return View("~/Views/Ajustes/PaginaAjuste.cshtml");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Se ha producido un error: {0}", ex);
+                return View("~/Views/Errores/paginaError.cshtml");
+            }
+        }
+
     }
 }
---
+++ LinkSocial1/DTO/ModificarContraseñaDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkSocial1.DTO
+{
+    public class ModificarContraseñaDTO
+    {
+        public int idUsuario { get; set; }
+
+        [Required(ErrorMessage = "El campo de la contraseña es obligatorio.")]
+        [RegularExpression(@"^(?=.*[a-zA-Z]).+$", ErrorMessage = "La contraseña debe incluir al menos una letra.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "La confirmación de la contraseña es obligatoria.")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string Password2 { get; set; }
+    }
+}
